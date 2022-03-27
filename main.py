c0bc75aeb32b9bf053db1e002a6bb1031849492a
Konstantin Starkov <60787729+kocteban@users.noreply.github.com>
2022-03-27 22:33:47 +0500

    start to develop bot logic

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import timeit
 bot = telebot.TeleBot('5236914607:AAH-9RQudTWP7K5UfYwhXT2RvEK9q0fdWMg')
 
 
-@bot.message_handlers(content_types=['text'])
+@bot.message_handler(content_types='text')
 def get_text_message(message):
-    if message == 'Какой сегодня праздник?':
-        bot.send_message()
+    if message.text.lower() == 'персикбот, какой сегодня праздник?':
+        bot.send_message(message.from_user.id, 'Такой то!')
+    else:
+        bot.send_message(message.from_user.id, 'Я вас не понял =(')
+
+
+
+
+
+
+bot.polling(none_stop=True, interval=0)
